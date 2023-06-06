c4da1e97789fb2cf8427f3b98168e34aed9b703d
Hillary Kiprotich <hillarykim001@gmail.com>
2023-06-06 16:02:21 +0300

    fixed the bug by removing the absolute value

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lDigit = abs(number) % 10
+if number >= 0:
+    lDigit = number % 10
+else:
+    lDigit = lDigit = ((number * -1) % 10) * -1
+
 if lDigit > 5:
     print(f"Last digit of {number} is {lDigit} and is greater than 5")
 elif lDigit == 0:
