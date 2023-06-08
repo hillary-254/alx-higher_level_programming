be04645c3ba85c1b9fd3116b3389c5c2d21ca200
Hillary Kiprotich <hillarykim001@gmail.com>
2023-06-08 16:58:04 +0300

    fixing bugs

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from variable_load_5.py import a
-
+"""
+imports the variable a from the file variable_load_5.py and prints its value
+"""
 if __name__ == '__main__':
     print(a)
