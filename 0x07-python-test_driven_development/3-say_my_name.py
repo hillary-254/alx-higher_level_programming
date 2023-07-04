4d1236190f8f515f0591fa59317269873ef592ef
Hillary Kiprotich <hillarykim001@gmail.com>
2023-07-04 18:55:52 +0300

    Function that prints My name is <first name> <last name>
    Prototype: def say_my_name(first_name, last_name=""):
    first_name and last_name must be strings otherwise, raise a TypeError exception with the message first_name must be a string or last_name must be a string

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the name in the format "My name is <first_name> <last_name>" or "My name is <first_name>".
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
+
+    Raises:
+        TypeError: If either `first_name` or `last_name` is not a string.
+
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
