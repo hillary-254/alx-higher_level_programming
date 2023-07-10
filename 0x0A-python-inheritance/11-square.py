54577b91d2834dea8e8d4fa4f09a2e539ef42cf4
Hillary Kiprotich <hillarykim001@gmail.com>
2023-07-10 23:12:44 +0300

    Write a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).
    Instantiation with size: def __init__(self, size)::
    size must be private. No getter or setter
    size must be a positive integer, validated by integer_validator
    the area() method must be implemented
    print() should print, and str() should return, the square description: [Square] <width>/<height>

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    """
+    A class representing a square.
+
+    This class inherits from the Rectangle class and provides functionality specific to squares.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(self, size): Initializes a Square instance with the given size.
+        area(self): Calculates and returns the area of the square.
+        __str__(self): Returns a string representation of the square in the format [Square] <size>/<size>.
+
+    """
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = 0
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: A string representation of the square in the format [Square] <size>/<size>.
+
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
