2610ebf086cb2db43a8a5caabc7816f0aeb01b54
Hillary Kiprotich <hillarykim001@gmail.com>
2023-07-11 13:50:06 +0300

    fixing bugs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python3
+"""Module that reads then prints a text file"""
+
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to be read. Defaults to an empty string.
+        filename (str): The name of the file to be read.
+        Defaults to an empty string.
 
     Returns:
         None
