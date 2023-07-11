4895d159bf706ef836dc5f1a1990fe6a46ecbe56
Hillary Kiprotich <hillarykim001@gmail.com>
2023-07-11 13:51:44 +0300

    fixing bugs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -13,6 +13,6 @@ def read_file(filename=""):
     Returns:
         None
     """
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
+    with open(filename) as f:
+        text = f.read()
+        print(text, end="")
