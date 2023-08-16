72939e5ea582a26dafbe1e1771164074bcbac9f4
hillary-254 <hillarykim001@gmail.com>
2023-08-16 20:13:14 +0300

    fixed the code

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,15 +1,4 @@
--- List all privileges of users user_0d_1 and user_0d_2 on localhost
-
--- Create user user_0d_1
-CREATE USER 'user_0d_1'@'localhost';
-
--- Grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Load privileges for user_0d_1
-SELECT * FROM mysql.user WHERE user='user_0d_1' AND host='localhost';
-
--- Create user user_0d_2
-CREATE USER 'user_0d_2'@'localhost';
-
--- Error: No privileges defined for user_0d_2
+-- Lists all privileges of MySQL Users `user_0d_1`
+-- and `user_0d_2` in `localhost`.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
