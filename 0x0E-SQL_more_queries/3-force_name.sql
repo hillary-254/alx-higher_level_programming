2ce5df1c1816e43e207b41fbd7dc1771e3cd285d
hillary-254 <hillarykim001@gmail.com>
2023-08-16 20:24:46 +0300

    fixed code error

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,18 +1,7 @@
 -- Create table force_name in the specified database
 
 -- Create table if not exists
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.force_name (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    name VARCHAR(256) NOT NULL
 );
-
--- Insert data
-INSERT INTO hbtn_0d_2.force_name (id, name) VALUES (89, "Best School");
-
--- Attempt to insert data without specifying 'name'
--- This will result in an error
-INSERT INTO hbtn_0d_2.force_name (id) VALUES (333);
-
--- Display data from force_name table
-SELECT * FROM hbtn_0d_2.force_name;
