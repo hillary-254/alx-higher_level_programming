6900a22da485785e5048c948063f5b192a9da834
hillary-254 <hillarykim001@gmail.com>
2023-09-12 14:59:54 +0300

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+exports.nbOccurences = function (list, searchElement) {
+    return list.reduce((count, element) => {
+      if (element === searchElement) {
+        return count + 1;
+      }
+      return count;
+    }, 0);
+  };
+  
