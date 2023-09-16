426229f59815193c705844e23024151763071702
hillary-254 <hillarykim001@gmail.com>
2023-09-16 16:38:29 +0300

    fixing error in codes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
+"""
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    cursor = db.cursor()
-    cursor.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities\
-                    JOIN states ON cities.state_id = states.id\
-                    WHERE states.name=%s ORDER BY cities.id ASC", (state_name,))
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
 
-    result = cursor.fetchone()[0]
-    print(result if result else "")
+        rows = cur.fetchall()
 
-    cursor.close()
-    db.close()
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
+        
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Prints the first State object from the database hbtn_0e_6_usa
+"""
+This script lists all State objects
+that contain the letter `a`
+from the database `hbtn_0e_6_usa`.
 """
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(username, password, database),
-                           pool_pre_ping=True)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    first_state = session.query(State).order_by(State.id).first()
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
-    else:
-        print("Nothing")
-
-    session.close()
+    for instance in session.query(State).filter(State.name.contains('a')):
+        print('{0}: {1}'.format(instance.id, instance.name))
