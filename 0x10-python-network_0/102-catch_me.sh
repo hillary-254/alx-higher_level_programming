319127d9566e15bd3c168a24b1a66c3fa1479b19
hillary-254 <hillarykim001@gmail.com>
2023-09-28 19:01:38 +0300

    fixing the code

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a request to 0.0.0.0:5000/catch_me that triggers the server to respond with "You got me!"
-
-curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" "$1"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin: HolbertonSchool" -d "user_id=98"
