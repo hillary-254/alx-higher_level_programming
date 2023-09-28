eb3f66c239bf1fb7ce4f8f5c537bcef9d047590f
hillary-254 <hillarykim001@gmail.com>
2023-09-28 18:37:20 +0300

    fixing code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body of a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | { read status; [ $status -eq 200 ] && curl -s "$1" || echo ""; }
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
