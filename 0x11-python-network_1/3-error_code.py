4fc6a5948877e47f05d53133bd14dba995465d8d
hillary-254 <hillarykim001@gmail.com>
2023-09-29 16:45:50 +0300

    3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module sends a request to a URL, displays the response body (decoded in utf-8), and handles HTTP errors.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
