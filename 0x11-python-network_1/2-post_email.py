753c5cd36e60e9610f105196c436268c74f5ce61
hillary-254 <hillarykim001@gmail.com>
2023-09-29 16:45:32 +0300

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module sends a POST request with an email parameter to a URL and displays the response body (decoded in utf-8).
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
